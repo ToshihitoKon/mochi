fcd3a581ac796494d1ad5030a77afb9c970914cb
ToshihitoKon <toshihito.kon@gmail.com>
2020-01-28 22:27:20 +0900

    出力をレスポンスに含めた

--- src/models/daifuku.py
+++ src/models/daifuku.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import json
 from flask import request
 
 root_path = '/home/pi'
@@ -9,19 +10,39 @@ def handler_mochi_pull():
     if not os.path.isfile(path):
         return 'error: ' + path + ' not found'
 
-    cp = subprocess.run(path)
+    cp = subprocess.run(path, capture_output=True, text=True)
     if cp.returncode != 0:
-        return 'error: ' + path + ' return not 0'
+        res = {
+            'status': 'error: ' + path + ' return not 0',
+            'stdout': cp.stdout,
+            'stderr': cp.stderr
+        }
+        return json.dumps(res, ensure_ascii=False)
 
-    return 'success'
+    res = {
+        'status': 'success',
+        'stdout': cp.stdout,
+        'stderr': cp.stderr
+    }
+    return json.dumps(res, ensure_ascii=False)
 
 def handler_kashiwa_pull():
     path = os.path.join(root_path, 'kashiwa', 'pull.sh')
     if not os.path.isfile(path):
         return 'error: ' + path + ' not found'
 
-    cp = subprocess.run(path)
+    cp = subprocess.run(path, capture_output=True, text=True)
     if cp.returncode != 0:
-        return 'error: ' + path + ' return not 0'
+        res = {
+            'status': 'error: ' + path + ' return not 0',
+            'stdout': cp.stdout,
+            'stderr': cp.stderr
+        }
+        return json.dumps(res, ensure_ascii=False)
 
-    return 'success'
+    res = {
+        'status': 'success',
+        'stdout': cp.stdout,
+        'stderr': cp.stderr
+    }
+    return json.dumps(res, ensure_ascii=False)
